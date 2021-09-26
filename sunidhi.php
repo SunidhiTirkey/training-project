d0725e832df7b8f1b00da18c53f22feb5c7bcf4e
SunidhiTirkey <sunidhi.tirkey2000@gmail.com>
2021-09-26 22:42:48 +0530

    Initial Commit

---
+++ sunidhi.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>My portfolio</title>
+    <link rel="stylesheet" href="sunidhi.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Otomanopee+One&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Tinos&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body>
+    <header><h1>Sunidhi Tirkey</h1></header>
+    <div class="class1">
+      <img
+        src="sunny.jpg"
+        alt="Sunidhi's profile picture"
+        class="src"
+        widh="180"
+        height="210"
+      />
+    </div>
+    <br />
+    <p>
+      1st year B.tech student at <b>Chandigarh University.</b>
+      <a href="https://www.cuchd.in/" target="_blank">
+        <img
+          src="https://www.eduopinions.com/wp-content/uploads/2018/03/Chandigarh-University-CU-logo.jpg"
+          alt="CU logo"
+          width="50"
+      /></a>
+    </p>
+    <div class="class2">
+      <h3><u>CV Highlights</u></h3>
+      <br />
+      <ol>
+        <li>Learning Web Development.</li>
+        <li>Learning DBMS.</li>
+        <li>Made a chat-bot using python.</li>
+        <li>Made a night patrolling robot on basis of IOT.</li>
+        <li>Knowledge of C,C++,JAVA,MySQL,Python and IOT.</li>
+      </ol>
+    </div>
+    <div class="class3">
+      <h3><u> Educational Qualifications </u></h3>
+      <table>
+        <tr>
+          <th>Qualification</th>
+          <th>Board</th>
+          <th>Percentage / CGPA</th>
+          <th>Year</th>
+        </tr>
+        <tr>
+          <td>10th Boards</td>
+          <td>CBSE</td>
+          <td>10 CGPA</td>
+          <td>2017</td>
+        </tr>
+        <tr>
+          <td>Itermediate</td>
+          <td>CBSE</td>
+          <td>65%</td>
+          <td>2020</td>
+        </tr>
+      </table>
+    </div>
+    <div class="class4">
+      <h3><u> Education </u></h3>
+      <ul>
+        <li>
+          Primary and Secondary Schooling at St. Michael's School.
+          <a href="http://www.stmichaels.edu.in/" target="_blank"
+            ><img
+              src="download (1).png"
+              alt="SMS logo"
+              class="src"
+              width="80"
+          /></a>
+        </li>
+        <li>
+          High Schooling at D.A.V. Public School Bariatu.
+          <a href="http://davbariatu.org/" target="_blank"
+            ><img
+              src="unnamed.png"
+              alt="DAV logo"
+              class="src"
+              width="100"
+          /></a>
+        </li>
+      </ul>
+    </div>
+    <br /><br />
+    <div class="class5">
+      <a href="hobby.html" target="_blank">My Hobbies</a>
+    </div>
+  </body>
+</html>
